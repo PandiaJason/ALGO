274ba2505e4a7d7324eed2330ebed47809df7e2d
PandiaJason <pandiajason@gmail.com>
2026-09-03 23:34:09 +0530

    config: update allowedDevOrigins hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["localhost:3000", "127.0.0.1:3000", "172.20.10.3:3000"],
+  allowedDevOrigins: ["localhost", "127.0.0.1", "172.20.10.3", "localhost:3000", "172.20.10.3:3000"],
 };
 
 export default nextConfig;
